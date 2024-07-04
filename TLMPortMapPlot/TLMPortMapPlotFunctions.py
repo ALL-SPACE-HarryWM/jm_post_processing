92f30b5b2649985ce3cfb3f655fde12bd3d235f7
ALL-SPACE-HarryWM <156800653+ALL-SPACE-HarryWM@users.noreply.github.com>
2024-07-04 13:27:20 +0100

    pep 8 linting

--- TLMPortMapPlot/TLMPortMapPlotFunctions.py
+++ TLMPortMapPlot/TLMPortMapPlotFunctions.py
@@ -1,28 +1,25 @@
 import matplotlib.backends.backend_pdf
-from matplotlib import cm, colors
-from scipy.stats import norm
 import os
-import glob
-import copy
 import csv
-import json
-import time
 import pandas as pd
 from pylab import *
-# import seaborn as sns
 from matplotlib.markers import MarkerStyle
 # definitions
+
+
 def find_measFiles(path, fileString, beam, freq_set, it):
     files = []
     for root, directories, file in os.walk(path):
         for file in file:
-            if (file.endswith(".csv")) == True:
+            if (file.endswith(".csv")) is True:
                 files.append(os.path.join(root, file))
     measFiles = []
     for i in range(len(files)):
-        if fileString in files[i] and 'eam' + str(beam) in files[i] and f'{freq_set}_GHz_4' in files[i] and f'teration_{it}' in files[i]:
+        if fileString in files[i] and 'eam' + str(beam) in files[i] and f'{freq_set}_GHz_4' in files[i] and \
+                f'teration_{it}' in files[i]:
             measFiles.append(files[i])
     return measFiles, files
+
 
 def load__measFiles(file_path):
     if os.path.getsize(file_path) > 2000:
@@ -41,7 +38,6 @@ def load__measFiles(file_path):
             meas_array_phase = meas_array[:, 1:][:, ::2]
             meas_frequencies = np.array(meas_info[index_start - 1])[::2].astype(float)
 
-
     # meas_params
     for i in range(len(meas_info) - 1):
         if len(meas_info[i]) > 1:
@@ -54,16 +50,19 @@ def load__measFiles(file_path):
                     meas_params[paramName] = meas_params[paramName][1:]
     return meas_info, meas_params, meas_array, meas_frequencies, meas_array_gain, meas_array_phase, paramName, i
 
-def plot_tlm_map(array_in, title, cmin, cmax, cstp, f_set, plot_no, tick_step, delta_pol, TLM_Type,axs,meas_frequencies,  align=False, col_map='jet'):
 
+def plot_tlm_map(array_in, title, cmin, cmax, cstp, f_set, plot_no, tick_step, delta_pol, TLM_Type, axs,
+                 meas_frequencies, align=False, col_map='jet'):
 
     # map tlm as a df (and roated if needed)
     if TLM_Type == 'Rx':
-        map_tlm_df = pd.read_csv(r'.\20240227_tlm_map_plotter\20221019_TLMCalInputs\Mrk1_S2000_TLM_RX_ArrayGeometry_V20062022_CalInfo_Orig.csv',header=1)
+        map_tlm_df = pd.read_csv(r'.\20240227_tlm_map_plotter\20221019_TLMCalInputs\/' +
+                                 r'Mrk1_S2000_TLM_RX_ArrayGeometry_V20062022_CalInfo_Orig.csv', header=1)
     elif TLM_Type == 'Tx':
-        map_tlm_df = pd.read_csv(r'.\20240227_tlm_map_plotter\20221019_TLMCalInputs\Mrk1_S2000_TLM_TX_ArrayGeometry_V20062022_CalInfo.csv',header=1)
+        map_tlm_df = pd.read_csv(r'.\20240227_tlm_map_plotter\20221019_TLMCalInputs\/' +
+                                 r'Mrk1_S2000_TLM_TX_ArrayGeometry_V20062022_CalInfo.csv', header=1)
 
-    if align == True:
+    if align is True:
         map_tlm_df[' Feed x [mm] shift'] = map_tlm_df[' Feed x [mm]'] - map_tlm_df[' Lens x [mm]']
         map_tlm_df[' Feed y [mm] shift'] = map_tlm_df[' Feed y [mm]'] - map_tlm_df[' Lens y [mm]']
         map_tlm_df[' Dual-Pol Probe rotation [deg]']
@@ -95,9 +94,11 @@ def plot_tlm_map(array_in, title, cmin, cmax, cstp, f_set, plot_no, tick_step, d
 
     # plot rfics
     if TLM_Type == 'Rx':
-        map_rfic = pd.read_csv(r'.\20240227_tlm_map_plotter\20221019_TLMCalInputs\MK1_RX_TLM_RFIC_Patch_Feed_mapping_RF.csv')
+        map_rfic = pd.read_csv(r'.\20240227_tlm_map_plotter\20221019_TLMCalInputs\/' +
+                               r'MK1_RX_TLM_RFIC_Patch_Feed_mapping_RF.csv')
     elif TLM_Type == 'Tx':
-        map_rfic = pd.read_csv(r'.\20240227_tlm_map_plotter\20221019_TLMCalInputs\MK1_TX_TLM_RFIC_Patch_Feed_Mapping.csv')
+        map_rfic = pd.read_csv(r'.\20240227_tlm_map_plotter\20221019_TLMCalInputs\/' +
+                               r'MK1_TX_TLM_RFIC_Patch_Feed_Mapping.csv')
     rfics = list(set(list(map_rfic['RFIC Number'])))
     print(rfics)
     for rfic in rfics:
@@ -105,10 +106,10 @@ def plot_tlm_map(array_in, title, cmin, cmax, cstp, f_set, plot_no, tick_step, d
         print(map_rfic_cut)
         patches = map_rfic_cut['Patch Number']
         for lens in [0, 1, 2]:
-            x_rfic = [];
+            x_rfic = []
             y_rfic = []
             for patch in patches:
-                if align == True:
+                if align is True:
                     x_rfic.append(map_tlm_df['x_new'][patch - 1 + lens * float(len(map_tlm_df)) / 3])
                     y_rfic.append(map_tlm_df['y_new'][patch - 1 + lens * float(len(map_tlm_df)) / 3])
                     axs[plot_no].text(map_tlm_df['x_new'][patch - 1 + lens * float(len(map_tlm_df)) / 3],
@@ -133,12 +134,14 @@ def plot_tlm_map(array_in, title, cmin, cmax, cstp, f_set, plot_no, tick_step, d
     # marker for odd ports
     m = MarkerStyle('D', fillstyle='left')
     m._transform.rotate_deg(map_tlm_df[' Dual-Pol Probe rotation [deg]'][i] + 45)
-    #print('QQQQQqqqq',rot[i])
+    # print('QQQQQqqqq',rot[i])
     # scatter plot for odd pol
     v = np.linspace(cmin, cmax, int((cmax - cmin) / cstp), endpoint=True)
     cmap_chosen = matplotlib.colormaps.get_cmap(col_map)
-    #print(v)
-    cntr = axs[plot_no].scatter(map_tlm_df[' Feed x [mm]'],map_tlm_df[' Feed y [mm]'], c=Z_trim, marker=m, s=200, edgecolors='black', linewidths=0.5, cmap=cmap_chosen,vmin=min(v), vmax=max(v), alpha=1.0)
+    # print(v)
+    cntr = axs[plot_no].scatter(map_tlm_df[' Feed x [mm]'], map_tlm_df[' Feed y [mm]'], c=Z_trim, marker=m, s=200,
+                                edgecolors='black', linewidths=0.5, cmap=cmap_chosen, vmin=min(v), vmax=max(v),
+                                alpha=1.0)
 
     # even pol
     Z_trim = Z[1::2]
@@ -149,17 +152,21 @@ def plot_tlm_map(array_in, title, cmin, cmax, cstp, f_set, plot_no, tick_step, d
     m._transform.rotate_deg(map_tlm_df[' Dual-Pol Probe rotation [deg]'][i] + 45)
 
     # scatter plot for even pol
-    cntr = axs[plot_no].scatter(map_tlm_df[' Feed x [mm]'],map_tlm_df[' Feed y [mm]'] , c=Z_trim, marker=m, s=200, edgecolors='black', linewidths=0.5, cmap=cmap_chosen,vmin=min(v), vmax=max(v), alpha=1.0)
+    cntr = axs[plot_no].scatter(map_tlm_df[' Feed x [mm]'], map_tlm_df[' Feed y [mm]'], c=Z_trim, marker=m, s=200,
+                                edgecolors='black', linewidths=0.5, cmap=cmap_chosen, vmin=min(v), vmax=max(v),
+                                alpha=1.0)
 
     # scatter plot (on-top) for delta pol
-    if delta_pol == True:
+    if delta_pol is True:
         m = MarkerStyle('s')
         m._transform.rotate_deg(map_tlm_df[' Dual-Pol Probe rotation [deg]'][i])
-        cntr = axs[plot_no].scatter(map_tlm_df[' Feed x [mm]'],map_tlm_df[' Feed y [mm]'] , c=(Z_trim_pol1 - Z_trim_pol2), marker=m, s=200, edgecolors='black',linewidths=0.5, vmin=min(v), vmax=max(v), cmap=cmap_chosen, alpha=1.0)
+        cntr = axs[plot_no].scatter(map_tlm_df[' Feed x [mm]'], map_tlm_df[' Feed y [mm]'],
+                                    c=(Z_trim_pol1 - Z_trim_pol2), marker=m, s=200, edgecolors='black',
+                                    linewidths=0.5, vmin=min(v), vmax=max(v), cmap=cmap_chosen, alpha=1.0)
 
     cbar = plt.colorbar(cntr)
     cbar.set_ticks(np.arange(min(v), max(v) + tick_step, tick_step))
-    axs[plot_no].set_xlabel('X [mm]');
+    axs[plot_no].set_xlabel('X [mm]')
     axs[plot_no].set_ylabel('Y [mm]')
     axs[plot_no].set_title(title)
     return Z, Z_trim, map_tlm_df
--- TLMPortMapPlot/TLMPortMapPlot_Rx_Tx.py
+++ TLMPortMapPlot/TLMPortMapPlot_Rx_Tx.py
@@ -1,41 +1,26 @@
-# -*- coding: utf-8 -*-
 """
-Created on Mon Dec 18 16:40:42 2023
-
-@author: jmitchell
+Plots the port maps with their gain and phase given the input data
+created on 04/07/2024
+author: Harry William Morris
 """
-# imports
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt;
 
-plt.rcParams['font.size'] = 12
-import matplotlib.backends.backend_pdf
-from matplotlib import cm, colors
-from scipy.stats import norm
-import os
-import glob
-import copy
-import csv
-import json
-import time
-from pylab import *
-# import seaborn as sns
-from matplotlib.markers import MarkerStyle
 from TLMPortMapPlotFunctions import *
 
-plt.close('all')
+plt.rcParams['font.size'] = 12
+TLM_Type = 'Tx'  # 'Rx'
 
-TLM_Type = 'Tx' #'Rx'
-
-# set-up
+# set-up cal files and input data
 file_path = r'.\2024-07-01_17-16-37_MCR1_Rig1_cal_QR00169_1_20_3820_Bias_0_Override_7_29.50_45C'
 if TLM_Type == 'Rx':
-    map_tlm_df = pd.read_csv(r'.\20240227_tlm_map_plotter\20221019_TLMCalInputs\Mrk1_S2000_TLM_RX_ArrayGeometry_V20062022_CalInfo_Orig.csv',header=1)
+    map_tlm_df = pd.read_csv(
+        r'.\20240227_tlm_map_plotter\20221019_TLMCalInputs\Mrk1_S2000_TLM_RX_ArrayGeometry_V20062022_CalInfo_Orig.csv',
+        header=1)
     freq_list = ['19.20']
 elif TLM_Type == 'Tx':
-    map_tlm_df = pd.read_csv(r'.\20240227_tlm_map_plotter\20221019_TLMCalInputs\Mrk1_S2000_TLM_TX_ArrayGeometry_V20062022_CalInfo.csv',header=1)
-    freq_list = ['29.50']#['27.50', '28.00', '28.50', '29.00', '29.50', '30.00', '30.50', '31.00']
+    map_tlm_df = pd.read_csv(
+        r'.\20240227_tlm_map_plotter\20221019_TLMCalInputs\Mrk1_S2000_TLM_TX_ArrayGeometry_V20062022_CalInfo.csv',
+        header=1)
+    freq_list = ['29.50']  # ['27.50', '28.00', '28.50', '29.00', '29.50', '30.00', '30.50', '31.00']
 align = True
 beam_list = [2]
 it = 1
@@ -45,7 +30,7 @@ for gain_phase in ['gain', 'phase']:
 
     for delta_pol in [False, True]:
 
-        if delta_pol == True:
+        if delta_pol is True:
             if gain_phase == 'gain':
                 vmax_std = 6.0
                 v_OP = 8.0
@@ -63,7 +48,7 @@ for gain_phase in ['gain', 'phase']:
                 v_spread_step = 0.01
                 tick_step = 45.0
 
-        if delta_pol == False:
+        if delta_pol is False:
             if gain_phase == 'gain':
                 vmax_std = 6.0
                 v_OP = 25.0
@@ -87,19 +72,20 @@ for gain_phase in ['gain', 'phase']:
             count = 0
             out_array = np.zeros([len(map_tlm_df) * 2, len(freq_list) * 2 * len(beam_list)])
 
-
             for beam in beam_list:
 
                 for freq_set in freq_list:
                     # find measurement files and load one of them to make an empty matrix
                     measFiles, files = find_measFiles(file_path, f_type, beam, freq_set, it)
-                    meas_info, meas_params, meas_array, meas_frequencies, meas_array_gain, meas_array_phase, paramName, i = load__measFiles(measFiles[0])
+                    (meas_info, meas_params, meas_array, meas_frequencies, meas_array_gain, meas_array_phase, paramName,
+                     i) = load__measFiles(measFiles[0])
                     meas_array_tot = meas_array_gain * 0.0
                     meas_array_list = []
 
                     # cycle through meas files to make average and stdev arrays
                     for measFile in measFiles:
-                        meas_info, meas_params, meas_array, meas_frequencies, meas_array_gain, meas_array_phase, paramName, i = load__measFiles(measFile)
+                        (meas_info, meas_params, meas_array, meas_frequencies, meas_array_gain, meas_array_phase,
+                         paramName, i) = load__measFiles(measFile)
                         if gain_phase == 'gain':
                             meas_array_list.append(meas_array_gain)
                         if gain_phase == 'phase':
@@ -113,14 +99,22 @@ for gain_phase in ['gain', 'phase']:
                     # make plots for OP or RFA
                     if 'OP' in f_type:
                         Z, Z_trim, map_tlm_df = plot_tlm_map(meas_array_av,
-                                     f'({f_type}) {gain_phase} (average), N = {len(measFiles)} \n Freq = {freq_set} GHz, Beam {beam}, Delta Pol={delta_pol}, Lens Align={align}',
-                                     -v_OP, v_OP, v_OP_step, float(meas_params['f_c']), 0, tick_step=tick_step,
-                                     delta_pol=delta_pol,TLM_Type=TLM_Type, axs = axs,meas_frequencies=meas_frequencies, align=align)
+                                                             f'({f_type}) {gain_phase} (average), N = {len(measFiles)} '
+                                                             f'\n Freq = {freq_set} GHz, Beam {beam}, Delta Pol='
+                                                             f'{delta_pol}, Lens Align={align}',
+                                                             -v_OP, v_OP, v_OP_step, float(meas_params['f_c']), 0,
+                                                             tick_step=tick_step,
+                                                             delta_pol=delta_pol, TLM_Type=TLM_Type, axs=axs,
+                                                             meas_frequencies=meas_frequencies, align=align)
                     elif 'RFA' in f_type:
                         Z, Z_trim, map_tlm_df = plot_tlm_map(meas_array_av,
-                                     f'({f_type}) {gain_phase} (average), N = {len(measFiles)} \n Freq = {freq_set} GHz, Beam {beam}, Delta Pol={delta_pol}, Lens Align={align}',
-                                     -v_RFA, v_RFA, v_RFA_step, float(meas_params['f_c']), 0, tick_step=tick_step,
-                                     delta_pol=delta_pol,TLM_Type=TLM_Type, axs = axs,meas_frequencies=meas_frequencies,  align=align)
+                                                             f'({f_type}) {gain_phase} (average), N = {len(measFiles)} '
+                                                             f'\n Freq = {freq_set} GHz, Beam {beam}, Delta Pol='
+                                                             f'{delta_pol}, Lens Align={align}',
+                                                             -v_RFA, v_RFA, v_RFA_step, float(meas_params['f_c']), 0,
+                                                             tick_step=tick_step,
+                                                             delta_pol=delta_pol, TLM_Type=TLM_Type, axs=axs,
+                                                             meas_frequencies=meas_frequencies, align=align)
 
                     # add to out_array
                     out_array[:, count] = Z
@@ -128,15 +122,20 @@ for gain_phase in ['gain', 'phase']:
                     # replace the decimal point in the frequency for filenames
                     freq_str = freq_set.replace('.', 'g')
 
-                    # plot the stdev
+                    # plot the stdev todo change x axis from ports to RFIC from data
                     Z, Z_trim, map_tlm_df = plot_tlm_map(meas_array_std,
-                                 f'({f_type}) {gain_phase} (stdev), N = {len(measFiles)} \n Freq = {freq_set} GHz, Beam {beam}, Delta Pol={delta_pol}, Lens Align={align}',
-                                 0.0, vmax_std, v_spread_step, float(meas_params['f_c']), 1, tick_step=tick_step,
-                                 delta_pol=delta_pol,TLM_Type=TLM_Type, axs = axs,meas_frequencies=meas_frequencies,  align=align)
+                                                         f'({f_type}) {gain_phase} (stdev), N = {len(measFiles)} '
+                                                         f'\n Freq = {freq_set} GHz, Beam {beam}, Delta Pol={delta_pol}'
+                                                         f', Lens Align={align}',
+                                                         0.0, vmax_std, v_spread_step, float(meas_params['f_c']), 1,
+                                                         tick_step=tick_step,
+                                                         delta_pol=delta_pol, TLM_Type=TLM_Type, axs=axs,
+                                                         meas_frequencies=meas_frequencies, align=align)
 
                     # plot the cartesian
                     # for measFile in measFiles:
-                    meas_info, meas_params, meas_array, meas_frequencies, meas_array_gain, meas_array_phase, paramName, i = load__measFiles(measFile)
+                    (meas_info, meas_params, meas_array, meas_frequencies, meas_array_gain, meas_array_phase,
+                     paramName, i) = load__measFiles(measFile)
                     col = np.argmin((meas_frequencies - float(freq_set)) ** 2)
                     axs[2].plot(np.linspace(1, len(meas_array), num=len(meas_array)), meas_array_av[:, col])
                     axs[2].set_xlabel('port')
@@ -144,7 +143,8 @@ for gain_phase in ['gain', 'phase']:
                     axs[2].set_xlim([0, 500])
                     axs[2].set_xticks(np.linspace(0, 500, num=int(500 / 50) + 1))
                     axs[2].set_title(
-                        f'({f_type}) {gain_phase} (average), N = {len(measFiles)} \n Freq = {freq_set} GHz, Beam {beam}, Lens Align={align}')
+                        f'({f_type}) {gain_phase} (average), N = {len(measFiles)} \n Freq = {freq_set} GHz, Beam {beam}'
+                        f', Lens Align={align}')
                     axs[2].grid('on')
 
                     # add the stdev to the outarray
@@ -169,7 +169,7 @@ for gain_phase in ['gain', 'phase']:
             header_list = []
             for beam in beam_list:
                 for i in range(len(freq_list)):
-                    header_list.append(freq_list_av[i] + f'_beam{beam}');
+                    header_list.append(freq_list_av[i] + f'_beam{beam}')
                     header_list.append(freq_list_std[i] + f'_beam{beam}')
             df = pd.DataFrame(out_array, columns=header_list)
             lens_list = (list(map_tlm_df['Lens no.']) + list(map_tlm_df['Lens no.']))
